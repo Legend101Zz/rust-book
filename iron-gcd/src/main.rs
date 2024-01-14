99044fcbf15c12990d142064ea4581191f02595a
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-01-14 11:40:55 +0530

    finishes

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
@@ -11,10 +14,27 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
+}
 
-fn main(){
+fn main() {
+    let mut numbers = Vec::new();
 
-let a= gcd(2,10);
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing"))
+    }
 
-println!("{}",a)
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
---
+++ iron-gcd/target/debug/deps/iron_gcd-304c1df8a47fc316.d
@@ -0,0 +1,5 @@
+/Users/comreton/Desktop/rust-bot/RUST-BOOK/iron-gcd/target/debug/deps/iron_gcd-304c1df8a47fc316: src/main.rs
+
+/Users/comreton/Desktop/rust-bot/RUST-BOOK/iron-gcd/target/debug/deps/iron_gcd-304c1df8a47fc316.d: src/main.rs
+
+src/main.rs:
